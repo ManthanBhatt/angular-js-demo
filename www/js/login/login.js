58a8ccddaf4738e94073a8b661fc9f9b23d1aafe
Manthan Bhatt <manthanbhatt6225@gmail.com>
2024-12-25 21:24:23 +0530

    completed login page functionality

--- www/js/login/login.js
+++ www/js/login/login.js
@@ -1,10 +1,14 @@
 // www/js/login/login.js
 angular.module('teamManager.login', [])
   .controller('LoginCtrl', function($scope, $state, $ionicPlatform) {
-    
     $scope.user = {};
+    $scope.errorMessage = "";
 
     $scope.login = function() {
+      if($scope.user.email !== 'admin@mail.com' || $scope.user.password !== "admin123") {
+        $scope.errorMessage = "Invalid email or password";
+        return;
+      }
       console.log('Logging in with', $scope.user);
       $state.go('home'); // Redirect to home page after login
     };
