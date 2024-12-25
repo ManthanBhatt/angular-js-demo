a123b4e3c5aae33e2f5d95e45b409c1fc0eed200
Manthan Bhatt <manthanbhatt6225@gmail.com>
2024-12-25 21:24:40 +0530

    added title case pipe

---
+++ www/js/pipes/titleCase.js
@@ -0,0 +1,7 @@
+function titleCase(input) {
+    var words = input.split(' ');
+    for (var i = 0; i < words.length; i++) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1).toLowerCase();
+    }
+    return words.join(' ');
+}
