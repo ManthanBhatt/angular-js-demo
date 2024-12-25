2ccb8c185c7f803650d3cbd18526fdfd55ad4d1a
Manthan Bhatt <manthanbhatt6225@gmail.com>
2024-12-25 21:24:15 +0530

    completed home page functionality

--- www/js/home/home.js
+++ www/js/home/home.js
@@ -1,13 +1,15 @@
 angular.module('teamManager.home', [])
-  .controller('HomeCtrl', function ($scope, dataService, $ionicPlatform) {
-    $scope.playerCount = dataService.getPlayers().length;
-    $scope.teamCount = dataService.getTeams().length;
-    $scope.matchCount = dataService.getMatches().length;
-
-    $ionicPlatform.ready(function() {
+  .controller('HomeCtrl', function ($scope, dataService) {
+    
+    function setData() {
       $scope.playerCount = dataService.getPlayers().length;
       $scope.teamCount = dataService.getTeams().length;
       $scope.matchCount = dataService.getMatches().length;
+    }
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+      if (toState.name === 'home') {
+        setData();
+      }
     });
- 
+
   });
